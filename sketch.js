5409b9d611e25454e6704a6db3b5198db24fa462
Umang balicha <54236975+umang331@users.noreply.github.com>
2020-11-16 13:42:35 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
-var Engine = Matter.Engine,
-  World = Matter.World,
-  Events = Matter.Events,
-  Bodies = Matter.Bodies;
+const Engine = Matter.Engine,
+const  World = Matter.World,
+const  Events = Matter.Events,
+const  Bodies = Matter.Bodies;
  
 
 var plinkos = [];
